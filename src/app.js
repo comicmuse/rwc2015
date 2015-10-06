127fef78d1c44b9108e243bd06420c290f825d12
comicmuse <colm.linehan@gmail.com>
2015-10-06 23:21:09 +0100

    Internal version with new in-play mode
    New In-Play mode, includes score and card details, and looks prettier. Addresses issues #2, #5, #6. Needs testing before release.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
+console.log ('Starting 1');
+
 // Create a Card with title and subtitle
 var card = new UI.Card({
   title:'RWC 2015',
@@ -19,6 +21,10 @@ card.on('click', 'select', function ()
 
 // Display the Card
 card.show();
+
+//define the inplay window to be used later
+var window = new UI.Window({fullscreen: true});
+
 
 reloadfrontpage();
 
@@ -37,6 +43,8 @@ ajax(
     console.log('Successfully fetched match schedule');
     var text ='';
     var i=0;
+    
+  
     while (i<data.matches.length && data.matches[i].status == 'C')
       {
         i++;
@@ -49,7 +57,11 @@ ajax(
         text = text + data.matches[i].time.label;  
       }
     //We're at half time
-    else if (data.matches[i].status=='LHT'|| data.matches[i].status=='L1' || data.matches[i].status == 'L2')
+    else if (data.matches[i].status=='LHT') 
+      {
+        halftime(data.matches[i].matchId);
+      }
+    else if (data.matches[i].status=='L1' || data.matches[i].status == 'L2')
       {
         inplay(data.matches[i].matchId);
       }
@@ -65,7 +77,6 @@ ajax(
     card.subtitle('');
     card.body(text);    
     
-    
   },
   function(error) {
     // Failure!
@@ -75,12 +86,138 @@ ajax(
 );
 }
 
+function halftime(matchId) {
+  //put some code here
+}
 
 function inplay(matchId) {
   //Set up some variables
   var team1, team2;
-  var teamlist1 = new Array();
-  var teamlist2 = new Array();
+  var playerlist = new Array();
+  //Set up the display
+  var Vector2 = require('vector2');
+  
+  var scorelayer = new UI.Text ({
+    position: new Vector2(0,0),
+    size: new Vector2(144, 30),
+    font: 'gothic-28-bold',
+    text: 'loading',
+    textAlign: 'center',
+    backgroundColor: 'white',
+    color: 'black'
+  });
+  var timelayer = new UI.Text ({
+    position: new Vector2(0,30),
+    size: new Vector2(144, 20),
+    font: 'gothic-14',
+    text: '',
+    textAlign: 'center',
+    backgroundColor: 'white',
+    color: 'black'    
+  });
+  var eventslayer = new UI.Text ({
+    position: new Vector2(0,50),
+    size: new Vector2(144, 94),
+    font: 'gothic-14',
+    text: '',
+    textAlign: 'left',
+  });  
+  
+  window.add(scorelayer);
+  window.add(timelayer);
+  window.add(eventslayer);
+  console.log('set up in-play window');
+  card.hide();
+  window.show();
+  console.log('showing in-play window');
+  //Utility Functions
+  function scoretemplate(score1, score2) {
+    return team1 + ' ' + score1 + '-' + score2 + ' ' + team2;
+  }
+  
+  function updateevents(data){
+    //extract all the events from the game, put them in an array, sort it backwards, and return it. 
+    var eventsArray = new Array();
+    var eventTimesArray = new Array();
+    for (var team =0 ; team < data.teams.length; team++ ) {
+      if (data.teams[team].scoring.Try !== undefined) {
+        for (var atry=0; atry < data.teams[team].scoring.Try.length; atry++) {
+          eventsArray[data.teams[team].scoring.Try[atry].time.secs] = data.teams[team].scoring.Try[atry].time.label + ': Try - ' + playerlist[data.teams[team].scoring.Try[atry].playerId];
+          eventTimesArray.push(data.teams[team].scoring.Try[atry].time.secs);
+        }
+      }
+      if (data.teams[team].scoring.Con !== undefined) {
+        for (var con=0; con < data.teams[team].scoring.Con.length; con++) {
+          eventsArray[data.teams[team].scoring.Con[con].time.secs] =  data.teams[team].scoring.Con[con].time.label + ': Con - ' + playerlist[data.teams[team].scoring.Con[con].playerId];
+          eventTimesArray.push(data.teams[team].scoring.Con[con].time.secs);
+        }
+      }
+      if (data.teams[team].scoring.Pen !== undefined) {
+        for (var pen=0; pen < data.teams[team].scoring.Pen.length; pen++) {
+          eventsArray[data.teams[team].scoring.Pen[pen].time.secs] =  data.teams[team].scoring.Pen[pen].time.label + ': Pen - ' + playerlist[data.teams[team].scoring.Pen[pen].playerId];
+          eventTimesArray.push(data.teams[team].scoring.Pen[pen].time.secs);
+        }
+      }
+      if (data.teams[team].scoring.DG !== undefined) {
+        for (var dg=0; dg < data.teams[team].scoring.DG.length; dg++) {
+          eventsArray[data.teams[team].scoring.DG[dg].time.secs] =  data.teams[team].scoring.DG[dg].time.label + ': DG - ' + playerlist[data.teams[team].scoring.DG[dg].playerId];
+          eventTimesArray.push(data.teams[team].scoring.DG[dg].time.secs);
+        }
+      }
+      
+      if (data.teams[team].disciplinary !== undefined) {
+        for (var yc=0; yc < data.teams[team].disciplinary.YC.length; yc++) {
+          eventsArray[data.teams[team].disciplinary.YC[yc].time.secs] =  data.teams[team].disciplinary.YC[yc].time.label + ': Bin - ' + playerlist[data.teams[team].disciplinary.YC[yc].playerId];
+          eventTimesArray.push(data.teams[team].disciplinary.YC[yc].time.secs);
+        }
+      }
+      if (data.teams[team].disciplinary.RC !== undefined) {
+        for (var rc=0; rc < data.teams[team].disciplinary.RC.length; rc++) {
+          eventsArray[data.teams[team].disciplinary.RC[rc].time.secs] =  data.teams[team].disciplinary.RC[rc].time.label + ': Off - ' + playerlist[data.teams[team].disciplinary.RC[rc].playerId];
+          eventTimesArray.push(data.teams[team].disciplinary.RC[rc].time.secs);
+        }
+      }
+    }
+    //ok, now sort the event times
+    eventTimesArray.sort(function (a, b) {return b-a;}); 
+    console.log('The eventTimesArray:' + eventTimesArray.toString());
+    
+    //construct a string of the events
+    var text = '';
+    for (var i = 0; i < eventTimesArray.length && i<8; i++) {
+      text = text + eventsArray[eventTimesArray[i]] + '\n';
+    }
+    console.log(text);
+    return text;
+  }
+  
+  
+  //few of statics so we minimise updates to the window
+  var lastscorelayer='';
+  var lasteventslayer='';
+  var lasttimelayer='';
+  
+  function updateinplaywindow(data){
+      var newscorelayer=scoretemplate(data.match.scores[0], data.match.scores[1]);
+      var neweventslayer=updateevents(data);
+      var newtimelayer=data.match.clock.label;
+      if (newscorelayer !== lastscorelayer) {
+        scorelayer.text(newscorelayer);
+        lastscorelayer=newscorelayer;
+        console.log ('updated scorelayer');
+      }
+      if (neweventslayer !== lasteventslayer) {
+        eventslayer.text(neweventslayer);
+        lasteventslayer=neweventslayer;
+        console.log ('updated eventslayer');
+      }
+      if (newtimelayer !== lasttimelayer) {
+        timelayer.text (newtimelayer);
+        lasttimelayer=newtimelayer;
+        console.log ('updated timelayer');
+      }
+      console.log ('Screen update requested but nothing to do');
+  }
   
   //Get initial match detail
   var URL = 'http://cmsapi.pulselive.com/rugby/match/' + matchId + '/summary?language=en&client=pebble';
@@ -90,59 +227,53 @@ function inplay(matchId) {
       type: 'json'
     },
     function(data) {
+      console.log('Got initial in-play match data');
       //Store the team names for ease of reference
       team1 = data.match.teams[0].abbreviation;
       team2 = data.match.teams[1].abbreviation;
-      
-      
-      //Cache the player lists for later use with scoring
-      for (var i=0; i<=data.teams[0].teamList.list; i++)
-      {
-        if (data.teams[0].teamList.list[i].player.name.last.known !== null) {
-          teamlist1[data.teams[0].teamList.list[i].player.id] = data.teams[0].teamList.list[i].player.name.last.known;
-        }
-        else {
-          teamlist1[data.teams[0].teamList.list[i].player.id] = data.teams[0].teamList.list[i].player.name.last.official;
-        }
-      }
-      for ( i=0; i<=data.teams[1].teamList.list; i++)
-      {
-        if (data.teams[1].teamList.list[i].player.name.last.known !== null) {
-          teamlist2[data.teams[1].teamList.list[i].player.id] = data.teams[1].teamList.list[i].player.name.last.known;
-        }
-        else {
-          teamlist2[data.teams[1].teamList.list[i].player.id] = data.teams[1].teamList.list[i].player.name.last.official;
+            
+      //Cache the player lists for later use
+      for (var team =0; team <=1; team++) {
+        for (var i=0; i<data.teams[team].teamList.list.length; i++)
+        {
+          if (data.teams[team].teamList.list[i].player.name.last.known !== null) {
+            playerlist[data.teams[team].teamList.list[i].player.id] = data.teams[team].teamList.list[i].player.name.last.known;
+          }
+          else {
+            playerlist[data.teams[team].teamList.list[i].player.id] = data.teams[team].teamList.list[i].player.name.last.official;
+          }
         }
       }
+      console.log ('Cached Player List:' + playerlist.length);
       
-      var text = data.match.teams[0].abbreviation + ' ' + data.match.scores[0] + '-' + data.match.scores[1] + ' ' + data.match.teams[1].abbreviation + '\n';
-      text = text + data.match.clock.label;
-      card.body (text);
-
-      
+      updateinplaywindow(data);
+            
       //Main In-Play Loop
       
-      var pointlessVar = setInterval (inplayloop, 60000);
+      var pointlessvar = setInterval (inplayloop, 60000);
+      
       function inplayloop (){
+        console.log('inplay loop is running');
         ajax(
         {
           url: URL,
           type: 'json'
         },
         function (data){
-          var text='';
           if (data.match.status=='C') {
-            text = 'FT:';
-            clearInterval(pointlessVar);
-          }
-          text = text + data.match.teams[0].abbreviation + ' ' + data.match.scores[0] + '-' + data.match.scores[1] + ' ' + data.match.teams[1].abbreviation + '\n';
-          text = text + data.match.clock.label;
-          card.body (text);
+            window.hide();
+            card.show();
+            clearInterval(pointlessvar);
+            console.log ('hopefully we have cleared the interval');
+            return;
+          } 
+          console.log('inplay loop updated json');
+          updateinplaywindow(data);
         },
         function (error) {
+          timelayer('Something went wrong. Please reopen');
           console.log('Failed updating match details: ' + error);
         });
-
       }
       //End of Main In-Play Loop
             
